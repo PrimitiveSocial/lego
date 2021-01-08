bdcd5f7518a9f3c827e79cd81ae6812e6e7ed005
Elie Andraos <elie@andraos.org>
2021-01-08 17:29:39 +0200

    :sparkles: Primary & secondary button components

---
+++ src/components/SecondaryButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        v-on="$listeners"
+        v-bind="$attrs"
+        :class="classList"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    data: () => ({
+        classList: ''
+    }),
+    created() {
+        let config = (window.Lego && typeof (window.Lego) === 'object') ? window.Lego : require('./../config/lego.config');
+        this.classList = config.secondaryButton;
+    }
+}
+</script>
