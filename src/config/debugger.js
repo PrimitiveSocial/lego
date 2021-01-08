9725fcc53f8052bb2adec3bb1dee394151908578
Elie Andraos <elie@andraos.org>
2021-01-08 17:55:04 +0200

    :loud_sound: Add debug messages

---
+++ src/config/debugger.js
@@ -0,0 +1,2 @@
+export const DROPDOWN_REQUIRED_VMODEL_AND_OR_OPTIONS_PROP = "The dropdown v-model and/or 'options' prop are not defined.";
+export const DROPDOWN_EMPTY_OPTIONS_PROP = "The 'options' prop is empty.";
