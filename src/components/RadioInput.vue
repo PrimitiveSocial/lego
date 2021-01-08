df53843ee27762e3f6bf713e1348753f6714cc8b
Elie Andraos <elie@andraos.org>
2021-01-08 17:30:02 +0200

    :sparkles: Radio input component

---
+++ src/components/RadioInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <div :class="classLists.container">
+        <div class="flex items-center" :class="classLists.wrapper" v-for="(option,index) in options" :key="index">
+            <label class="flex items-center cursor-pointer">
+                <input type="radio" class="hidden" :value="option.value" v-model="selected" v-bind="$attrs" />
+
+                <span class="inline-block flex-no-shrink" :class="classLists.radio">
+                    <span :class="getRadioClasses(option)"></span>
+                </span>
+                <span class="select-none">{{ option.label }}</span>
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        value: {
+            required: true,
+        },
+        options: {
+            type: Array,
+            required: true,
+        }
+    },
+    data: () => {
+        return {
+            selected: null,
+            classLists: {},
+        }
+    },
+    methods: {
+        getRadioClasses(option) {
+            let radioBgColor = (option.value === this.selected) ? this.classLists.innerRadioSelected : '';
+            return this.classLists.innerRadio + ' ' + radioBgColor;
+        }
+    },
+    watch: {
+        isChecked: {
+            handler: function() {
+                this.$emit('input', this.isChecked);
+            },
+            immediate: true
+        }
+    },
+    created() {
+        let config = ( window.Lego && typeof (window.Lego) === 'object' ) ? window.Lego : require('./../config/lego.config');
+        this.classLists = config.radioInput;
+
+        this.selected = this.value;
+    }
+}
+</script>
