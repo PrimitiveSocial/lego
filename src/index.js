825e75f43c3e09710b3a3b2f824b4a23d1df3567
Elie Andraos <elie@andraos.org>
2021-01-08 17:35:07 +0200

    :truck: Ship out components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import Icon from "./components/Icon";
-import TextInput from "./components/TextInput";
+import Tab from "@/components/Tab";
+import Tabs from "@/components/Tabs";
+import Icon from "@/components/Icon";
+import Toggle from "@/components/Toggle";
+import Dropdown from "@/components/Dropdown";
+import TextInput from "@/components/TextInput";
+import RadioInput from "@/components/RadioInput";
+import CheckboxInput from "@/components/CheckboxInput";
+import PrimaryButton from "@/components/PrimaryButton";
+import SecondaryButton from "@/components/SecondaryButton";
 
 const VueLego = {
     install(Vue) {
+        Vue.component("tab", Tab);
         Vue.component("icon", Icon);
+        Vue.component("tabs", Tabs);
+        Vue.component("toggle", Toggle);
+        Vue.component("dropdown", Dropdown);
         Vue.component("text-input", TextInput);
+        Vue.component("radio-input", RadioInput);
+        Vue.component("checkbox-input", CheckboxInput);
+        Vue.component("primary-button", PrimaryButton);
+        Vue.component("secondary-button", SecondaryButton);
     }
 };
 
