5dce39763c68b231b1c38a5420fcb9358eb89d71
Elie Andraos <elie@andraos.org>
2021-01-08 17:06:03 +0200

    :sparkles: Text input component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <input
+        :type="type"
+        :class="classList"
+        :value="value"
+        v-on="$listeners"
+        v-bind="$attrs"
+        @input="$emit('update', $event.target.value)"
+    />
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        value: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: false,
+            default: 'text',
+        }
+    },
+    model: {
+        prop: "value",
+        event: "update"
+    },
+    data: () => {
+        return {
+            classList: '',
+        }
+    },
+    created() {
+        let config = ( window.Lego && typeof (window.Lego) === 'object' ) ? window.Lego : require('./../config/lego.config');
+
+        let { textinput } = config;
+        this.classList = textinput;
+    }
+}
+</script>
